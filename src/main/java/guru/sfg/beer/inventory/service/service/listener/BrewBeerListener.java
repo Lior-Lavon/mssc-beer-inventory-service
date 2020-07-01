7b2afaa2534b57facbb3496d5ebd506a49836e0d
Lior Lavon <>
2020-07-01 20:42:56 +0200

    add jms support and listener for new brew

---
+++ src/main/java/guru/sfg/beer/inventory/service/service/listener/BrewBeerListener.java
@@ -0,0 +1,33 @@
+package guru.sfg.beer.inventory.service.service.listener;
+
+import guru.sfg.beer.inventory.service.config.JmsConfig;
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
+import guru.sfg.common.events.BeerDTO;
+import guru.sfg.common.events.NewInventoryEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class BrewBeerListener {
+
+    private final BeerInventoryRepository beerInventoryRepository;
+
+    @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
+    public void listen(NewInventoryEvent event){
+
+        log.debug("Got Inventory: " + event.toString());
+
+        BeerDTO beerDto = event.getBeerDto();
+        beerInventoryRepository.save(BeerInventory.builder()
+                .beerId(beerDto.getId())
+                .upc(beerDto.getUpc())
+                .quantityOnHand(beerDto.getQuantityOnHand())
+                .build());
+    }
+}
